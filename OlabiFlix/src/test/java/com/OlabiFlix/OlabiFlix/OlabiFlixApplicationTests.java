637b86f66c25da67560bf7feebad5b362af10393
Cristina Sousa <Cristina.wep2013@gmail.com>
2024-05-31 18:29:24 -0300

    Constucao uma API Spring Boot Rest usando Spring Data JPA com banco de dados H2

---
+++ OlabiFlix/src/test/java/com/OlabiFlix/OlabiFlix/OlabiFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OlabiFlix.OlabiFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlabiFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
